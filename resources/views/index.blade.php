2f866e5b49f262b82b4671cd27434cbb668bd1ea
asmi046 <asmi046@gmail.com>
2026-06-10 17:25:39 +0300

    ссылки в заглушках

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,23 +25,24 @@
                 </div>
 
                 <div class="placeholder__bottom" aria-label="Магазины на маркетплейсах">
-                    <a class="placeholder__marketplace" href="https://www.ozon.ru/" target="_blank"
-                        rel="noopener noreferrer" aria-label="Магазин Курский Лес на Ozon">
+                    <a class="placeholder__marketplace" href="https://www.ozon.ru/brand/devinir-100931078/?__rr=1"
+                        target="_blank" rel="noopener noreferrer" aria-label="Магазин Курский Лес на Ozon">
                         <img class="placeholder__marketplace-logo" src="{{ asset('/img/marketplaces/ozon_logo.svg') }}"
                             alt="Ozon">
                         <img class="placeholder__marketplace-qr" src="{{ asset('/img/marketplaces/ozon.png') }}"
                             alt="QR-код Ozon магазина">
                     </a>
 
-                    <a class="placeholder__marketplace" href="https://market.yandex.ru/" target="_blank"
-                        rel="noopener noreferrer" aria-label="Магазин Курский Лес на Яндекс Маркете">
+                    <a class="placeholder__marketplace"
+                        href="https://market.yandex.ru/business--ooo-kurskii-les/173188186?utm_medium=sharing&generalContext=t%3DshopInShop%3Bi%3D1%3Bbi%3D173188186%3B&rs=eJwz4v_EyMPBKLDwEKsEg8a5I6wAJTkEmQ%2C%2C&searchContext=sins_ctx"
+                        target="_blank" rel="noopener noreferrer" aria-label="Магазин Курский Лес на Яндекс Маркете">
                         <img class="placeholder__marketplace-logo" src="{{ asset('/img/marketplaces/YM_logo.svg') }}"
                             alt="Яндекс Маркет">
                         <img class="placeholder__marketplace-qr" src="{{ asset('/img/marketplaces/ym.png') }}"
                             alt="QR-код магазина Яндекс Маркета">
                     </a>
 
-                    <a class="placeholder__marketplace" href="https://www.wildberries.ru/" target="_blank"
+                    <a class="placeholder__marketplace" href="https://www.wildberries.ru/seller/1209707" target="_blank"
                         rel="noopener noreferrer" aria-label="Магазин Курский Лес на Wildberries">
                         <img class="placeholder__marketplace-logo" src="{{ asset('/img/marketplaces/wb_logo.svg') }}"
                             alt="Wildberries">
